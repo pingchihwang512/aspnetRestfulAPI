a6abbe074a45a5dbc2dfeb2b6ba3673d83c9624f
BensonWang512 <pingchihwang512@gmail.com>
2024-05-31 16:57:45 -0700

    Update
    Learning LINQ

---
+++ .NetCoreAPIStudy/FakeXiechange.API/Controllers/ShoudongAPIController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FakeXiechange.API.Controllers
+{
+    [Route("api/shoudongapi")]
+    public class ShoudongAPIController : ControllerBase
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "valu2" };
+        }
+    }
+}
