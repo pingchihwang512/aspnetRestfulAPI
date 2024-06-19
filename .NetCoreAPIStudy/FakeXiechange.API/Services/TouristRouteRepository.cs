7d9cf79de4dcf604b844bf6161d3b8a447c4591a
BensonWang512 <pingchihwang512@gmail.com>
2024-06-18 17:45:41 -0700

    connect db
    connect db

---
+++ .NetCoreAPIStudy/FakeXiechange.API/Services/TouristRouteRepository.cs
@@ -0,0 +1,30 @@
+﻿using FakeXiechange.API.Database;
+using FakeXiecheng.API.Models;
+using FakeXiecheng.API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FakeXiechange.API.Services
+{
+    public class TouristRouteRepository : ITouristRouteRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TouristRouteRepository(AppDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public TouristRoute GetTouristRoute(Guid touristRouteId)
+        {
+            return _context.TouristRoutes.FirstOrDefault(n => n.Id == touristRouteId);
+        }
+
+        public IEnumerable<TouristRoute> GetTouristRoutes()
+        {
+            return _context.TouristRoutes;
+        }
+    }
+}
--- .NetCoreAPIStudy/FakeXiechange.API/Startup.cs
+++ .NetCoreAPIStudy/FakeXiechange.API/Startup.cs
@@ -10,21 +10,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using FakeXiechange.API.Services;
 
 namespace FakeXiechange.API
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddTransient<ITouristRouteRepository, MockTouristRouteRepository>();
+            services.AddTransient<ITouristRouteRepository, TouristRouteRepository>();
             //services.AddSingleton
             //services.AddScoped
             services.AddDbContext<AppDbContext>(options => {
-                options.UseSqlServer("");
+                //options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FakeXiechengDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+                //options.UseSqlServer("server=localhost; Database=FakeXichengDb; User Id = sa; Password=PaSSword12!;");
+                options.UseSqlServer(Configuration["DbContext:ConnectionString"]);
             });
         }
 
